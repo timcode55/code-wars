7792321bb4ec3a0693b60d6bd013a5c18a960060
Tim Petrvalsky <tpet55@yahoo.com>
2021-10-17 10:01:32 -0700

    Leetcode practice

--- app.js
+++ app.js
@@ -691,8 +691,15 @@
 // 	[ 2, -6, 8, 1 ]
 // ]);
 
-function wordsToSentence(words) {
-	return words.join(' ');
-}
+// function wordsToSentence(words) {
+// 	return words.join(' ');
+// }
 
-console.log(wordsToSentence([ 'hello', 'world' ])); //, 'hello world');
+// console.log(wordsToSentence([ 'hello', 'world' ])); //, 'hello world');
+
+const nums = [1, 3, 2, 1];
+var getConcatenation = function (nums) {
+  return nums.concat(nums);
+};
+
+console.log(getConcatenation(nums));
