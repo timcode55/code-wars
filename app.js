e0cc103c6c59c4de2d9c7801d3115d92eca6eb8b
Tim Petrvalsky <tpet55@yahoo.com>
2021-12-03 08:44:22 -0700

    Codewars practice

--- app.js
+++ app.js
@@ -1772,15 +1772,34 @@
 // console.log(bump("_nnnnnnn_n__n______nn__nn_nnn")); //, "Car Dead");
 // console.log(bump("______n___n_")); //, "Woohoo!");
 
-function getDecimal(n) {
-  let array = n.toString().split(".");
-  if (array.length <= 1) {
-    return 0;
+// function getDecimal(n) {
+//   let array = n.toString().split(".");
+//   if (array.length <= 1) {
+//     return 0;
+//   }
+//   let result = `0.${array[1]}`;
+//   return +result;
+// }
+
+// console.log(getDecimal(10)); //, 0);
+// console.log(getDecimal(-1.2)); //, 0.2);
+// console.log(getDecimal(4.5)); //, 0.5);
+
+function mergeArrays(a, b) {
+  let result = [];
+  for (let i = 0; i < Math.max(a.length, b.length); i++) {
+    a[i] !== undefined ? result.push(a[i]) : null;
+    b[i] !== undefined ? result.push(b[i]) : null;
   }
-  let result = `0.${array[1]}`;
-  return +result;
+  return result;
 }
 
-console.log(getDecimal(10)); //, 0);
-console.log(getDecimal(-1.2)); //, 0.2);
-console.log(getDecimal(4.5)); //, 0.5);
+console.log(mergeArrays([1, 2, 3, 4, 5, 6, 7, 8], ["a", "b", "c", "d", "e"])); //, [1, "a", 2, "b", 3, "c", 4, "d", 5, "e", 6, 7, 8]);
+console.log(mergeArrays(["a", "b", "c", "d", "e"], [1, 2, 3, 4, 5])); //, ['a', 1, 'b', 2, 'c', 3, 'd', 4, 'e', 5]);
+console.log(mergeArrays([2, 5, 8, 23, 67, 6], ["b", "r", "a", "u", "r", "s"])); //, [2, 'b', 5, 'r', 8, 'a', 23, 'u', 67, 'r', 6, 's']);
+console.log(
+  mergeArrays(
+    ["b", "r", "a", "u", "r", "s", "e", "q", "z"],
+    [2, 5, 8, 23, 67, 6]
+  )
+); //, ['b', 2, 'r', 5, 'a', 8, 'u', 23, 'r', 67, 's', 6, 'e', 'q', 'z']);
