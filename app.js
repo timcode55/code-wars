21a0057216323c087c4b5cba2915bfc128ebbb52
Tim Petrvalsky <tpet55@yahoo.com>
2021-12-16 14:56:15 -0700

    Codewars practice

--- app.js
+++ app.js
@@ -1933,24 +1933,50 @@
 // console.log(adjacentElementsProduct([1, 0, 1, 0, 1000])); //, 0);
 // console.log(adjacentElementsProduct([1, 2, 3, 0])); //, 6);
 
-function iTri(s) {
-  if (s === 0) return "Starting Line... Good Luck!";
-  if (s > 0 && s <= 2.4) {
-    return { Swim: `${(140.6 - s).toFixed(2)} to go!` };
+// function iTri(s) {
+//   if (s === 0) return "Starting Line... Good Luck!";
+//   if (s > 0 && s <= 2.4) {
+//     return { Swim: `${(140.6 - s).toFixed(2)} to go!` };
+//   }
+//   if (s > 2.4 && s <= 114.4) {
+//     return { Bike: `${(140.6 - s).toFixed(2)} to go!` };
+//   }
+//   if (s > 114.4 && s < 130.6) {
+//     return { Run: `${(140.6 - s).toFixed(2)} to go!` };
+//   }
+//   if (s >= 130.6 && s < 140.6) {
+//     return { Run: "Nearly there!" };
+//   }
+//   return "You're done! Stop running!";
+// }
+
+// console.log(iTri(36)); //,{'Bike':'104.60 to go!'});
+// console.log(iTri(103.5)); //,{'Bike':'37.10 to go!'});
+// console.log(iTri(0)); //,'Starting Line... Good Luck!');
+// console.log(iTri(2)); //,{'Swim':'138.60 to go!'});
+
+function lastSurvivor(letters, coords) {
+  let array = letters.split("");
+  for (let item of coords) {
+    array.splice(item, 1);
   }
-  if (s > 2.4 && s <= 114.4) {
-    return { Bike: `${(140.6 - s).toFixed(2)} to go!` };
-  }
-  if (s > 114.4 && s < 130.6) {
-    return { Run: `${(140.6 - s).toFixed(2)} to go!` };
-  }
-  if (s >= 130.6 && s < 140.6) {
-    return { Run: "Nearly there!" };
-  }
-  return "You're done! Stop running!";
+  return array[0];
 }
 
-console.log(iTri(36)); //,{'Bike':'104.60 to go!'});
-console.log(iTri(103.5)); //,{'Bike':'37.10 to go!'});
-console.log(iTri(0)); //,'Starting Line... Good Luck!');
-console.log(iTri(2)); //,{'Swim':'138.60 to go!'});
+console.log(lastSurvivor("abc", [1, 1])); //, "a");
+console.log(lastSurvivor("kbc", [0, 1])); //, "b");
+console.log(lastSurvivor("zbk", [2, 1])); //, "z");
+console.log(lastSurvivor("c", [])); //, "c");
+console.log(
+  lastSurvivor(
+    "foiflxtpicahhkqjswjuyhmypkrdbwnmwbrrvdycqespfvdviucjoyvskltqaqirtjqulprjjoaiagobpftywabqjdmiofpsr",
+    [
+      8, 59, 52, 93, 21, 40, 88, 85, 59, 10, 82, 18, 74, 59, 51, 47, 75, 49, 23,
+      56, 1, 33, 39, 33, 34, 44, 25, 0, 51, 25, 36, 32, 57, 10, 57, 12, 51, 55,
+      24, 55, 31, 49, 6, 15, 10, 48, 27, 29, 38, 30, 35, 42, 23, 32, 9, 39, 39,
+      36, 8, 29, 2, 33, 14, 3, 13, 25, 9, 25, 18, 10, 1, 2, 20, 8, 2, 11, 5, 7,
+      0, 10, 10, 8, 12, 3, 5, 1, 7, 7, 5, 1, 4, 0, 4, 0, 0, 1
+    ]
+  )
+); //,
+("d");
