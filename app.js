8c8f456a76885a63e93c4c48523e822764ee45ea
Tim Petrvalsky <timsmac@Tims-14-MacBook-Pro.local>
2021-12-19 14:34:28 -0700

    Codewars practice

--- app.js
+++ app.js
@@ -1955,28 +1955,38 @@
 // console.log(iTri(0)); //,'Starting Line... Good Luck!');
 // console.log(iTri(2)); //,{'Swim':'138.60 to go!'});
 
-function lastSurvivor(letters, coords) {
-  let array = letters.split("");
-  for (let item of coords) {
-    array.splice(item, 1);
-  }
-  return array[0];
-}
+// function lastSurvivor(letters, coords) {
+//   let array = letters.split("");
+//   for (let item of coords) {
+//     array.splice(item, 1);
+//   }
+//   return array[0];
+// }
 
-console.log(lastSurvivor("abc", [1, 1])); //, "a");
-console.log(lastSurvivor("kbc", [0, 1])); //, "b");
-console.log(lastSurvivor("zbk", [2, 1])); //, "z");
-console.log(lastSurvivor("c", [])); //, "c");
+// console.log(lastSurvivor("abc", [1, 1])); //, "a");
+// console.log(lastSurvivor("kbc", [0, 1])); //, "b");
+// console.log(lastSurvivor("zbk", [2, 1])); //, "z");
+// console.log(lastSurvivor("c", [])); //, "c");
+// console.log(
+//   lastSurvivor(
+//     "foiflxtpicahhkqjswjuyhmypkrdbwnmwbrrvdycqespfvdviucjoyvskltqaqirtjqulprjjoaiagobpftywabqjdmiofpsr",
+//     [
+//       8, 59, 52, 93, 21, 40, 88, 85, 59, 10, 82, 18, 74, 59, 51, 47, 75, 49, 23,
+//       56, 1, 33, 39, 33, 34, 44, 25, 0, 51, 25, 36, 32, 57, 10, 57, 12, 51, 55,
+//       24, 55, 31, 49, 6, 15, 10, 48, 27, 29, 38, 30, 35, 42, 23, 32, 9, 39, 39,
+//       36, 8, 29, 2, 33, 14, 3, 13, 25, 9, 25, 18, 10, 1, 2, 20, 8, 2, 11, 5, 7,
+//       0, 10, 10, 8, 12, 3, 5, 1, 7, 7, 5, 1, 4, 0, 4, 0, 0, 1
+//     ]
+//   )
+// ); //,
+// ("d");
+
+const convertHashToArray = (hash) => Object.entries(hash).sort();
+
+console.log(convertHashToArray({ name: "Jeremy" })); //,[["name", "Jeremy"]]);
+console.log(convertHashToArray({ name: "Jeremy", age: 24 })); //,[["age", 24], ["name", "Jeremy"]]);
 console.log(
-  lastSurvivor(
-    "foiflxtpicahhkqjswjuyhmypkrdbwnmwbrrvdycqespfvdviucjoyvskltqaqirtjqulprjjoaiagobpftywabqjdmiofpsr",
-    [
-      8, 59, 52, 93, 21, 40, 88, 85, 59, 10, 82, 18, 74, 59, 51, 47, 75, 49, 23,
-      56, 1, 33, 39, 33, 34, 44, 25, 0, 51, 25, 36, 32, 57, 10, 57, 12, 51, 55,
-      24, 55, 31, 49, 6, 15, 10, 48, 27, 29, 38, 30, 35, 42, 23, 32, 9, 39, 39,
-      36, 8, 29, 2, 33, 14, 3, 13, 25, 9, 25, 18, 10, 1, 2, 20, 8, 2, 11, 5, 7,
-      0, 10, 10, 8, 12, 3, 5, 1, 7, 7, 5, 1, 4, 0, 4, 0, 0, 1
-    ]
-  )
-); //,
-("d");
+  convertHashToArray({ name: "Jeremy", age: 24, role: "Software Engineer" })
+); //,[["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]);
+console.log(convertHashToArray({ product: "CodeWars", powerLevelOver: 9000 })); //,[["powerLevelOver", 9000], ["product", "CodeWars"]]);
+console.log(convertHashToArray({}), []);
