0512cd986c4335d61f734e6cdc967e2fa643f6dc
Tim Petrvalsky <tpet55@yahoo.com>
2021-12-29 22:57:08 -0600

    Codewars practice

--- app.js
+++ app.js
@@ -2075,23 +2075,34 @@
 
 // console.log(PowerSet("abc"));
 
-function productArray(numbers) {
-  let result = [];
+// function productArray(numbers) {
+//   let result = [];
+//   for (let i = 0; i < numbers.length; i++) {
+//     let array = [...numbers];
+//     array.splice(i, 1);
+//     result.push(
+//       array.reduce((acc, val) => {
+//         return acc * val;
+//       })
+//     );
+//   }
+//   return result;
+// }
+
+// console.log(productArray([12, 20])); //, [20,12]);
+// console.log(productArray([12, 20])); //, [20,12]);
+// console.log(productArray([3, 27, 4, 2])); //, [216,24,162,324]);
+// console.log(productArray([13, 10, 5, 2, 9])); //, [900,1170,2340,5850,1300]);
+// console.log(productArray([16, 17, 4, 3, 5, 2])); //, [2040,1920,8160,10880,6528,16320]);
+
+function evenLast(numbers) {
+  let result = 0;
   for (let i = 0; i < numbers.length; i++) {
-    let array = [...numbers];
-    array.splice(i, 1);
-    // console.log(array, "ARRAY");
-    result.push(
-      array.reduce((acc, val) => {
-        return acc * val;
-      })
-    );
+    if (i % 2 === 0) {
+      result += numbers[i] * numbers[numbers.length - 1];
+    }
   }
   return result;
 }
 
-console.log(productArray([12, 20])); //, [20,12]);
-console.log(productArray([12, 20])); //, [20,12]);
-console.log(productArray([3, 27, 4, 2])); //, [216,24,162,324]);
-console.log(productArray([13, 10, 5, 2, 9])); //, [900,1170,2340,5850,1300]);
-console.log(productArray([16, 17, 4, 3, 5, 2])); //, [2040,1920,8160,10880,6528,16320]);
+console.log(evenLast([2, 3, 4, 5])); //, 30)
